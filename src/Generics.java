058a310561b09123704d8f600577e31b5fe1abef
RickyZhaoRuiQi <zhaoruiqi@hotmail.com>
2018-10-17 17:33:13 +0800

    test

--- src/Generics.java
+++ src/Generics.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+/*
+ *泛型类
+ * class GeneralType<Type>
+ * {...}
+ *
+ * 泛型方法
+ * class GeneralMethod
+ * {
+ *      <Type> void printClassName(Type object)
+ *      {
+ *          System.out.println(object.getClass().getName());
+ *      }
+ * }
+ *
+ * 使用通配符
+ * class GeneralType<Type>
+ * {...}
+ * class ShowType
+ * {
+ *      public void show(GeneralType<?> o)
+ *      {
+ *          System.out.println(o.getObj().getClass().getName());
+ *      }
+ * }
+ *
+ */
 
-public class Generics {
+public class Generics
+{
 }
