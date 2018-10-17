4c7d12f3bce568778ed45f17e491850174deedbf
RickyZhaoRuiQi <zhaoruiqi@hotmail.com>
2018-10-17 17:39:37 +0800

    Main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,22 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("hello world");
+        /*隐藏和覆盖测试*/
+        /*hideAndCover test = new hideAndCover();
+        test.printb();
+        test.setx(7);
+        test.printa();
+        test.printb();*/
+
+        /*相等和判同一*/
+        /*String s1 = new String("hello world");
+        String s2 = new String("hello world");
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        System.out.println(s1.getClass().getName());*/
+
+
     }
 }
